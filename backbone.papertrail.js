0276cacc18e02f6c2bfaf6d1db9200cde2b39e0b
Moudy <moudy.elkammash@gmail.com>
2012-07-21 12:04:07 -0400

    cleanup

--- backbone.papertrail.js
+++ backbone.papertrail.js
@@ -1,14 +1,14 @@
-(function(_, Backbone){
+(function (_, Backbone) {
+
   var PaperTrail = Backbone.PaperTrail = Backbone.Model.extend({
 
-    store: function(options) {
+    store: function (options) {
       options || (options = {});
 
-      var attrs = _.clone(this.attributes);
+      var attrs = _.clone(this.attributes),
+          same = _.isEqual(attrs, _.last(this.edits()));
 
-      if ( !_.isEqual(attrs, _.last(this.edits())) ) {
-        this.edits().push(attrs);
-      }
+      if (!same) this.edits().push(attrs);
 
       return this;
     },
@@ -16,12 +16,14 @@
     restore: function (options) {
       options || (options = {});
 
-      var index = options._index || 0;
+      var index = options._index || 0,
+          attrs = this.edits()[index];
 
-      var attrs = this.edits()[index];
       if (!attrs) return this;
 
-      for (var attr in this.attributes) attrs[attr] || this.unset(attr);
+      for (var attr in this.attributes) {
+        if (!attrs[attr]) this.unset(attr);
+      }
 
       this._edits = this.edits().slice(0, index);
 
@@ -31,37 +33,37 @@
     rollback: function (distance) {
       distance || (distance = 1);
 
-      var index = (this.edits()[0] ? this.edits().length - distance : 0);
+      var index = this.edits()[0] ? (this.edits().length - distance) : 0;
       if (index < 0) index = 0;
 
-      return this.restore({_index:index});
+      return this.restore({ _index: index });
     },
 
-    wasEdited: function() {
+    wasEdited: function () {
       return !!this.edits()[0];
     },
 
-    edits: function() {
+    edits: function () {
       return this._edits || (this._edits = []);
     },
 
     priorAttributes: function (options) {
-      var result, index;
       options || (options = {});
-      options.distance || (options.distance = 1);
 
-      index = (this.edits()[0] ? this.edits().length - options.distance : 0);
-      if (result = this.edits()[index]) return result;
+      var distance = options.distance || 1,
+          index = this.edits()[0] ? (this.edits().length - distance) : 0;
+
+      return this.edits()[index];
     },
 
     prior: function (attr, options) {
-      var result;
       options || (options = {});
-      options.distance || (options.distance = 1);
 
-      var index = (this.edits()[0] ? this.edits().length - options.distance : 0);
+      var distance = options.distance || 1,
+          index = this.edits()[0] ? this.edits().length - distance : 0,
+          result = this.edits()[index];
 
-      if (result = this.edits()[index]) return result[attr];
+      if (result) return result[attr];
     }
 
   });
